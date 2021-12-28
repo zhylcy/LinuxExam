297d106dd5d92b836bfdbd03268e770b2cbb739e
root <2514853837@qq.com>
2021-12-29 02:43:22 +0800

    UpdateStudent.java

---
+++ UpdateStudent.java
@@ -0,0 +1,112 @@
+package com.example;
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.google.gson.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import com.google.gson.reflect.TypeToken;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/UpdateStudent")
+public class UpdateStudent extends HttpServlet {
+   final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+   final static String URL = "jdbc:mysql://180.76.142.42/linux_final";
+   final static String USER = "root";
+   final static String PASS = "Zhylcy1101?";
+   final static String SQL_UPDATE_STUDENT = "UPDATE t_student SET name= ? , age = ? WHERE id=?";
+
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+
+      Student req = getRequestBody(request);
+      PrintWriter out = response.getWriter();
+
+      out.println(updateStudent(req));
+      out.flush();
+      out.close();
+   }
+
+   private Student getRequestBody(HttpServletRequest request) throws IOException {
+Student stu = new Student();
+      StringBuffer bodyJ = new StringBuffer();
+      String line = null;
+      BufferedReader reader = request.getReader();
+      while ((line = reader.readLine()) != null)
+         bodyJ.append(line);
+      Gson gson = new Gson();
+      stu = gson.fromJson(bodyJ.toString(), new TypeToken<Student>() {
+      }.getType());
+      return stu;
+   }
+
+   private int updateStudent(Student req) {
+      Connection conn = null;
+      PreparedStatement stmt = null;
+      int retcode = -1;
+      try {
+         Class.forName(JDBC_DRIVER);
+         conn = DriverManager.getConnection(URL, USER, PASS);
+         stmt = conn.prepareStatement(SQL_UPDATE_STUDENT);
+
+         stmt.setString(1, req.name);
+         stmt.setInt(2, req.age);
+         stmt.setInt(3, req.id);
+
+         int row = stmt.executeUpdate();
+         if (row > 0)
+            retcode = 1;
+
+         stmt.close();
+         conn.close();
+      } catch (SQLException se) {
+         se.printStackTrace();
+ } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            if (stmt != null)
+               stmt.close();
+            if (conn != null)
+               conn.close();
+         } catch (SQLException se) {
+            se.printStackTrace();
+         }
+      }
+      return retcode;
+   }
+public class Student {
+    int id;
+    String name;
+    int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+                                                           this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age =age;
+ }   }}
+
+                                                  
+
